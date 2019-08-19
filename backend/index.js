4e460a34c4a57164762f24ce398e9dc2da693f91
hakimks <hakimks@gmail.com>
2019-08-19 17:11:43 +0300

    feat Added first dragon class sample class

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("Hello Node");
+const Dragon = require('./dragon.js');
+
+const fooey = new Dragon({
+    birthdate: new Date(), 
+    nickname: 'fooey'
+});
+const baloo = new Dragon({
+    birthdate: new Date(), 
+    nickname: 'baloo'
+});
+
+console.log('fooey', fooey);
+console.log('baloo', baloo);
+
