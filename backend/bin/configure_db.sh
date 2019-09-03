68e3339d812258175ee5b9bfc9aa1492f0cfe2c6
hakimks <hakimks@gmail.com>
2019-09-03 13:13:00 +0300

    added scipt file to create the databse tables

--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+export PGPASSWORD="node_password";
+echo "Configuring the dragonstackdb"
+dropdb -U node_user -h localhost dragonstackdb
+createdb -U node_user -h localhost dragonstackdb
+psql -U node_user -h localhost dragonstackdb < ./bin/sql/generation.sql
+psql -U node_user -h localhost dragonstackdb < ./bin/sql/dragon.sql
+echo "dragonstackdb was configured"
