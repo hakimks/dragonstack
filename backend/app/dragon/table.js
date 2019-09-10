dc0ea87490c8d857e30aa7b8b64ab15f7f5f9ff7
hakimks <hakimks@gmail.com>
2019-09-10 11:07:17 +0300

    Feat: added traits table to configuration file to add traits to the database

--- backend/app/dragon/table.js
+++ backend/app/dragon/table.js
@@ -19,7 +19,7 @@ class DragonTable{
                     
                     resolve({ dragonId });
                 }
-            )
+            );
         });
     }
 }
--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -5,4 +5,8 @@ dropdb -U node_user -h localhost dragonstackdb
 createdb -U node_user -h localhost dragonstackdb
 psql -U node_user -h localhost dragonstackdb < ./bin/sql/generation.sql
 psql -U node_user -h localhost dragonstackdb < ./bin/sql/dragon.sql
+psql -U node_user -h localhost dragonstackdb < ./bin/sql/trait.sql
+
+node ./bin/insertTraits.js
+
 echo "dragonstackdb was configured"
