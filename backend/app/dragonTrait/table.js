d2c6c0cea074c65e3af01478ebcb45688a7e6919
hakimks <hakimks@gmail.com>
2019-09-11 18:35:53 +0300

    fix: added a promise that had to call the resolve method

--- backend/app/dragonTrait/table.js
+++ backend/app/dragonTrait/table.js
@@ -3,22 +3,23 @@ const TraitTable = require('../trait/table');
 
 class DragonTraitTable{
     static storeDragonTrait({dragonId, traitType, traitValue}){
-        TraitTable.getTraitId({traitType, traitValue})
-        .then(({ traitId})=> {
-            pool.query(
-                'INSERT INTO dragonTrait("traitId", "dragonId") VALUES ($1, $2)',
-                [traitId, dragonId],
-                (error, response) => {
-                    if(error) return reject(error);
+        return new Promise((resolve, reject) => {
+            TraitTable.getTraitId({traitType, traitValue})
+            .then(({ traitId})=> {
+                pool.query(
+                    'INSERT INTO dragonTrait("traitId", "dragonId") VALUES ($1, $2)',
+                    [traitId, dragonId],
+                    (error, response) => {
+                        if(error) return reject(error);
+    
+                        resolve();
+                    }
+                );
+    
+            });
 
-                    resolve();
-                }
-            );
-
-        } );
-
+        });    
     }
-
 }
 
 module.exports = DragonTraitTable;
