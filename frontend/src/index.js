97f56f73f814b715ba305d3c0531bf4723ed074a
hakimks <hakimks@gmail.com>
2019-09-24 17:14:38 +0300

    feat: Added redux and created a sample generation store and reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { createStore } from 'redux';
 import { render } from 'react-dom';
 import Generation from './components/Generation';
 import Dragon from './components/dragon';
 
 import './index.css'
+
+const DEFAULT_GENERATION = { generationId: '', expiration: ''};
+
+const generationReducer = (state, action)=>{
+    console.log('generarionRedider state', state);
+    console.log('generarionReducer action', action);
+
+    if(action.type === 'GENERATION_ACTION_TYPE'){
+        return { generation: action.generation}
+    }
+    
+    return { generation:  DEFAULT_GENERATION };
+}
+
+const store = createStore(generationReducer);
+
+store.dispatch({type: 'foo'});
+
+store.dispatch({
+    type: 'GENERATION_ACTION_TYPE',
+    generation: {generationId: 'goo', expiration: 'bar'}
+});
+
+
 
 render(
     <div>
